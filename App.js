21a2f3f1945a279cbbe4c770e328473b80677bf4
Eliandre <96908551+magrigsdev@users.noreply.github.com>
2025-11-12 14:33:06 +0100

    style(welcome): appliquer un fond blanc et styliser l'écran
    - Appliquer un fond blanc pour améliorer la clarté visuelle
    - Centrer le contenu de l'écran pour une meilleure présentation
    - Redimensionner les images et le bouton pour une cohérence visuelle

--- App.js
+++ App.js
@@ -1,21 +1,10 @@
 
-import { StyleSheet,  View } from 'react-native';
 import WelcomeScreen from './screens/welcomeScreen';
 
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <WelcomeScreen/>
-    </View>
+      <WelcomeScreen/> 
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
