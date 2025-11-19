d2e267a01166f51a74e383c8102a54d8127f97fd
Eliandre <96908551+magrigsdev@users.noreply.github.com>
2025-11-19 07:13:31 +0100

    feat(welcome): creation , mise en forme et  application LF

---
+++ screens/welcomeScreen.jsx
@@ -0,0 +1,27 @@
+import { Text, View,Image, Pressable } from "react-native"
+import { style } from './../styles/welcomeStyle';
+const WelcomeScreen = () => {
+    return (
+        <View style={style.main}>
+        
+            <Image 
+            source={{ uri:'https://cdn-icons-png.flaticon.com/512/3081/3081559.png'}}
+            style={style.logo}  />
+            <View style={style.contain}>
+                <Text style={style.text_2}>Bienvenue sur Eliandre shop</Text>
+                <Text style={style.text_1}>Découvrez Eliandre Shop, votre boutique en ligne dédiée à l’élégance et à la beauté.</Text>
+            </View>
+            <Image 
+                source={require('../assets/bienvenue.jpg')}
+                resizeMode="contain"
+                style={style.bienvenue}
+                />
+            <Pressable 
+                    onPress={()=>{}}
+                    style={style.button}
+                >
+                    <Text style={style.buttonText}>Commencez</Text>
+            </Pressable>
+        </View>)
+}
+export default WelcomeScreen
