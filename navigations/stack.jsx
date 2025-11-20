4dfbe4d5d8a571aa2d5c3a7980ad0d6b4566ae81
Eliandre <96908551+magrigsdev@users.noreply.github.com>
2025-11-20 12:42:35 +0100

    style(stack) mise à jour

--- navigations/stack.jsx
+++ navigations/stack.jsx
@@ -13,11 +13,13 @@ const Stack = () => {
         <NavigationContainer>
             <Stacks.Navigator initialRouteName="welcome">
                 <Stacks.Screen name="welcome" component={WelcomeScreen} 
-                    
+                    options={{ title :"Bienvenue"}}
                 />
                 <Stacks.Screen name="signup" component={SingupScreen}
+                options={{ title :"connexion" }}
                 />
                 <Stacks.Screen name="register" component={RegisterScreen}
+                options={{ title :"Création d'un compte" }}
                 />
             </Stacks.Navigator> 
         </NavigationContainer>    
