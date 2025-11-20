05d743b673d055cccb815fc280b2ebc090bffe2c
Eliandre <96908551+magrigsdev@users.noreply.github.com>
2025-11-20 12:50:21 +0100

    feat(database) creation des fichier task et initbd

--- database/db.js
+++ database/db.js
@@ -1,19 +1,6 @@
-
 import * as SQLite from 'expo-sqlite';
-const db = SQLite.openDatabase('monapp.db');
 
-export const initDB = () => {
-    db.transaction(tx => {
-        tx.executeSql(
-            `CREATE TABLE IF NOT EXISTS users (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            nom TEXT,
-            prenom TEXT,
-            telephone INTEGER,
-            email TEXT UNIQUE,
-            password TEXT,
-            );`
-        );
-    });
+export const openDB = async () => {
+  const db = await SQLite.openDatabaseAsync('MydbInstaF2');
+  return db;
 };
-export default db;
