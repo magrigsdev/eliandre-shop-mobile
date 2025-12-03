700a5980a43df7450be5d89c8556ffaf8744893b
Eliandre <96908551+magrigsdev@users.noreply.github.com>
2025-12-03 11:09:23 +0100

    feat(panier) l'ecran panier presque fini

--- context/panierProvider.jsx
+++ context/panierProvider.jsx
@@ -5,14 +5,12 @@ const PanierContext = createContext()
 
 export const PanierProvider = ({children}) => {
     //list des objets
-    const [panier, setPanier] = useState([]) 
+    const [panier, setPanier] = useState([])
 
     //ajoute le produit ou increment sa quantité
     const ajoutePanier = useCallback((item)=>{
       setPanier(prev => {
-            
             const exist = prev.find(p => p._id === item._id);
-            // const exist = (panier || []).find(p => p._id === item._id);
             if (exist) {
                 return prev.map(p =>
                     p._id === item._id ? { ...p, quantity: p.quantity + 1 } : p
@@ -23,17 +21,16 @@ export const PanierProvider = ({children}) => {
     },[])
 
     //calcule le total des articles
-    const totalItems = useMemo(()=>panier?.reduce((sum, p) => sum + (p.quantity || 0), 0), [panier])
+    const totalItems = useMemo(
+      ()=>panier?.reduce((sum, p) => sum + (p.quantity || 0), 0), [panier])
 
     //total des produit dans le panier
     const totalPrice = useMemo(
-          ()=> {
+          ()=>  
             //(panier || [])
-            //panier.reduce
-            (panier || []).reduce((sum, p) => 
-            sum + (p.quantity || 0) * (p.prix ?? p.price ?? 0), 0)
-          }
-        
+            //panier.reduce : init 
+            panier?.reduce((sum, p) => 
+            sum + (p.quantity || 0) * (p.prix ?? p.price ?? 0), 0).toFixed(2)
       ,[panier])
 
     //obtenir la quantité courant
@@ -51,16 +48,16 @@ export const PanierProvider = ({children}) => {
     //supprimer un element dans le panier
     /** @param id */
     const deleteOneFromPanier = useCallback((item)=>{
-      setPanier(
-        prev => {
-              prev.map(p=>p._id === item._id 
-                ? {...p, quantity: p.quantity - 1 } : p
-                ).filter(p=>p.quantity > 0)
-            } 
-      )
-      console.debug("degugage")
-      console.log(panier)
-      console.log(totalItems)
+      // setPanier(
+      //   // prev => {
+      //   //       prev.map(p=>p._id === item._id 
+      //   //         ? {...p, quantity: p.quantity - 1 } : p
+      //   //         ).filter(p=>p.quantity > 0)
+      //   //     } 
+        
+      // )
+      console.log("en cours de ceonception")
+      // totalPrice=0
     }, [])
 
    
